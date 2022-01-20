5efa0b2a70e56bfa00c1d6d8614598dc84cb3f91
Ibaihima <ibaihima@hotmail.com>
2022-01-19 21:33:05 -0500

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names, event) {
+    const newArray = []
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    
+    }
+
+    return newArray;
+}
+function countDown(){
+    let count = 10
+    while (count >= 0){
+        console.log (count--)
+    }
+
+}
